65b75a417c7bfaea30df551c6c6a46a5cd291764
tatielirb <tatielirb@gmail.com>
2024-02-08 16:48:10 -0300

    feat: create component button, and add new test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.scss';
-import Header from 'components/Header/Index';
+import React from "react";
+import "./App.scss";
+import Header from "components/Header/Index";
+import Button from "components/Button/Index";
 
 function App() {
   return (
     <div className="App">
-      <Header title="Car Refinancing Simulation"/>
-     
+      <Header title="Car Refinancing Simulation" />
+      <div>
+        <Button classNameType="btn fw-bold" title="Submit Application" />
+      </div>
     </div>
   );
 }
