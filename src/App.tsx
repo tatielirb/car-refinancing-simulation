6aef8c4d919a6cc9f359f1a6b4451c203110d7b4
tatielirb <tatielirb@gmail.com>
2024-02-15 14:22:02 -0300

    feat: initial arctetura the form page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import "./App.scss";
 import Header from "components/Header/Index";
 import Button from "components/Button/Index";
-import Alert from "components/Alert/Index";
+
 function App() {
+  const [query, setQuery] = useState("");
   return (
     <div className="App">
       <Header title="Car Refinancing Simulation" />
-      <Alert
-        message="Sorry! We had a problem with our service. Try again later!"
-        type="danger"
-        iconName="exclamation-triangle-fill"
-      />
-      <div>
-        <Button classNameType="btn fw-bold" title="Submit Application" />
+      <div className="personal-form container text-center">
+        <form className="row justify-content-md-center">
+          <div className="col col-3">
+            <div className="form-floating mb-5">
+              <select
+                className="form-select"
+                id="floatingSelect"
+                aria-label="Floating label select example"
+              >
+                <option>loanPurposeOption</option>
+              </select>
+              <label htmlFor="floatingSelect">Loan Purpose*</label>
+            </div>
+          </div>
+          <div className="col col-3">
+            <div className="form-floating mb-5">
+              <input
+                className="form-control"
+                id="floatingInputValue"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <label htmlFor="floatingInputValue">Total loan Amount*</label>
+            </div>
+          </div>
+          <div className="col col-3">
+            <div className="form-floating mb-5">
+              <select
+                className="form-select"
+                id="floatingSelect"
+                aria-label="Floating label select example"
+              >
+                <option>loanTermOption</option>
+              </select>
+              <label htmlFor="floatingSelect">Loan term(months)*</label>
+            </div>
+          </div>
+
+          <Suspense>
+            <div className="personal-form--fees">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <p className="personal-form--fees-title">Monthly payment</p>
+                  <span className="personal-form--fees-value">
+                    monthlyPayments
+                  </span>
+                </li>
+                <li className="list-group-item">
+                  <p className="personal-form--fees-title">APR</p>
+                  <p className="personal-form--fees-value">apr%</p>
+                </li>
+              </ul>
+            </div>
+          </Suspense>
+
+          <Button classNameType="btn fw-bold" title="Submit Application" />
+        </form>
       </div>
     </div>
   );
