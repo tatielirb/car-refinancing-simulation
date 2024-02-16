9199589eb9e389f57f257967f8714d5acc391786
tatielirb <tatielirb@gmail.com>
2024-02-16 17:17:05 -0300

    feat: services consumer

--- src/types/Offers.ts
+++ src/types/Offers.ts
@@ -8,3 +8,12 @@ export interface OffersPayload {
     monthlyPayments: number;
     apr: number;
   }
+  export interface SubmissionsPayload {
+    loanPurpose: string;
+    amount: number;
+    terms: number;
+    offerId: string;
+  }
+  export interface SubmissionsResponse {
+    userId: string;
+  }
